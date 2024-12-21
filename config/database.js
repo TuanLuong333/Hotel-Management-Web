89a7b2037bc773455fcc62d464049667c86434f5
TuanLuong333 <tuanbk0111@gmail.com>
2024-12-21 19:43:24 +0700

    refix config

--- config/database.js
+++ config/database.js
@@ -1,10 +1,36 @@
-// config/database.js
+// // config/database.js
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize('hotelmanage', 'root', '123456', {
+//     host: 'localhost',
+//     dialect: 'mysql'
+// });
+
+// // Đảm bảo bạn xuất khẩu sequelize
+// module.exports = sequelize;
+
+// // Kết nối tới cơ sở dữ liệu
+// module.exports.connect = async () => {
+//     try {
+//         await sequelize.authenticate();
+//         console.log('Kết nối thành công đến MySQL với Sequelize!');    
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('hotelmanage', 'root', '123456', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+// Sử dụng biến môi trường để cấu hình
+const sequelize = new Sequelize(
+    process.env.MYSQL_DATABASE, // Tên cơ sở dữ liệu
+    process.env.MYSQL_USER,     // Tên người dùng
+    process.env.MYSQL_PASSWORD, // Mật khẩu
+    {
+        host: process.env.MYSQL_HOST, // Host của MySQL
+        port: process.env.MYSQL_PORT,// Cổng mặc định của MySQL
+        dialect: 'mysql',
+    }
+);
 
 // Đảm bảo bạn xuất khẩu sequelize
 module.exports = sequelize;
@@ -13,8 +39,8 @@ module.exports = sequelize;
 module.exports.connect = async () => {
     try {
         await sequelize.authenticate();
-        console.log('Kết nối thành công đến MySQL với Sequelize!');    
+        console.log('Kết nối thành công đến MySQL với Sequelize!');
     } catch (error) {
-        console.log(error);
+        console.error('Không thể kết nối đến cơ sở dữ liệu:', error);
     }
 };
