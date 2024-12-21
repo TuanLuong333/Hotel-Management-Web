1337a9b0adaf179924f2acd7803fd58825111d5b
TuanTuVinhPhuc <trantuann1809@gmail.com>
2024-12-21 20:54:26 +0700

    hehe

--- public/client/js/image.js
+++ public/client/js/image.js
@@ -9,21 +9,18 @@ const images = [
 ];
 const totalImages = images.length;
 
-// Hàm thay đổi ảnh nền
 function changeBackgroundImage() {
   const img = new Image();
   img.src = images[index];
   img.onload = () => {
     document.body.style.backgroundImage = `url('${img.src}')`;
-    index = (index + 1) % totalImages; // Chuyển sang ảnh tiếp theo
+    index = (index + 1) % totalImages; 
   };
   img.onerror = () => {
     console.error(`Không thể tải ảnh: ${images[index]}`);
   };
 }
 
-// Cài đặt khoảng thời gian thay đổi ảnh nền
 setInterval(changeBackgroundImage, 3000);
 
-// Gọi hàm lần đầu để hiển thị ảnh đầu tiên
 changeBackgroundImage();
